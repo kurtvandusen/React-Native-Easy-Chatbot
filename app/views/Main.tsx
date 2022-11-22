ed21cf9ce4b2a6b225d9b890069bdb49d04023f7
kurtvandusen <kurtvandusen@gmail.com>
2022-11-22 14:06:12 -0500

    fix: add useCallback to onSend

--- app/views/Main.tsx
+++ app/views/Main.tsx
@@ -7,9 +7,9 @@ export default function Main() {
     const dispatch = useDispatch()   
     const messages = useSelector((state: MessagesState) => state.messages)
 
-    const onSend = (messages: IMessage[] = []) => {
+    const onSend = useCallback((messages = []) => {
         dispatch({ type: sendMessage.toString(), payload: messages[0]})
-    }
+      }, [])
 
     return (
         <GiftedChat
