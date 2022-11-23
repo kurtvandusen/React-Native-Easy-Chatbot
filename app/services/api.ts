d69ee0fe51cf57669ad69cf58cc9e12fdb733df9
kurtvandusen <kurtvandusen@gmail.com>
2022-11-23 11:16:09 -0500

    feat: add error handling to API

--- app/services/api.ts
+++ app/services/api.ts
@@ -30,19 +30,42 @@ const instance = axios.create({
   baseURL: "https://api-inference.huggingface.co/models/deepset/tinyroberta-squad2",
   headers: {
     "Authorization": "Bearer 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",
-    timeout: 10000,
+    timeout: 20000, // 20 seconds
   }
 });
 
 const responseBody = (response: AxiosResponse) => response.data;
 
+const handleError = (error) => console.log("API Error:", error)
+
+const handlePostError = (error) => {
+  handleError(error)
+  let defaultResponse: QuestionAnswerReturn = {
+    answer: `I don't know. My API feels fuzzy.`,
+    score: .5,
+    start: 0,
+    end: 0,
+  }
+  return defaultResponse
+}
+
 const requests = {
-	// get: (url: string) => instance.get(url).then(responseBody),
-	post: (url: string, body: QuestionAnswerArgs) => instance.post(url, body).then(responseBody),
-	// put: (url: string, body: {}) => instance.put(url, body).then(responseBody),
-	// delete: (url: string) => instance.delete(url).then(responseBody),
+	// get: (url: string) => instance.get(url).then(responseBody.catch(handleError),
+	post: (url: string, body: QuestionAnswerArgs) => instance.post(url, body).then(responseBody).catch(handlePostError),
+	// put: (url: string, body: {}) => instance.put(url, body).then(responseBody).catch(handleError),
+	// delete: (url: string) => instance.delete(url).then(responseBody).catch(handleError),
 };
 
 export const api = {
 	getAnswer: (body): Promise<QuestionAnswerReturn> => requests.post('', body)
 };
+
+ 
+axios.get('Web URL')
+.then(function(response) {
+    // handle response
+}).catch(function(error) {
+    // handle error
+}).finally(function() {
+    // always executes at the last of any API call
+});
