2891dcaca33388f7d63bb3f349a4a0d64941628e
kurtvandusen <kurtvandusen@gmail.com>
2022-11-23 12:29:11 -0500

    chore: add semantic-release husky eslint prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    extends: ["universe/native", "universe/shared/typescript-analysis"],
+
+    overrides: [
+        {
+            files: ["*.ts", "*.tsx", "*.d.ts"],
+
+            parserOptions: {
+                project: "./tsconfig.json",
+            },
+        },
+    ],
+
+    env: {
+        node: true,
+    },
+};
