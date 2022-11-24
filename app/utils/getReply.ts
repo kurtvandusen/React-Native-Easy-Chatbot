c73af3e8e2c138721ac10020446b41321fe1d272
kurtvandusen <kurtvandusen@gmail.com>
2022-11-24 13:09:58 -0500

    refactor: rename context as text

--- app/utils/getReply.ts
+++ app/utils/getReply.ts
@@ -2,14 +2,17 @@ import { IMessage } from "react-native-gifted-chat";
 
 import { api } from "../services/api";
 import { chatbotUser } from "../store/messages/messagesSlice";
-import { context } from "../utils/context";
+import { text } from "../utils/text";
 import { newId } from "./newId";
 
 export const getReply = async (message: IMessage) => {
   const body = {
     inputs: {
       question: message.text,
-      context,
+      context: text,
+    },
+    options: {
+      wait_for_model: true,
     },
   };
   const reply = await api.getAnswer(body);
