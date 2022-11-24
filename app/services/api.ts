b9a6b1ee8d8723ed9b9cadd4d2b0cbe37c4a1e7f
kurtvandusen <kurtvandusen@gmail.com>
2022-11-24 13:10:35 -0500

    feat: add axios-retry

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,42 +1,14 @@
 import axios, { AxiosResponse, AxiosError } from "axios";
+import axiosRetry from "axios-retry";
 import Constants from "expo-constants";
-
-interface IErrorBase<T> {
-  error: Error | AxiosError<T>;
-  type: "axios-error" | "stock-error";
-}
-
-interface IAxiosError<T> extends IErrorBase<T> {
-  error: AxiosError<T>;
-  type: "axios-error";
-}
-interface IStockError<T> extends IErrorBase<T> {
-  error: Error;
-  type: "stock-error";
-}
-
-export function axiosErrorHandler<T>(
-  callback: (err: IAxiosError<T> | IStockError<T>) => void
-) {
-  return (error: Error | AxiosError<T>) => {
-    if (axios.isAxiosError(error)) {
-      callback({
-        error,
-        type: "axios-error",
-      });
-    } else {
-      callback({
-        error,
-        type: "stock-error",
-      });
-    }
-  };
-}
 
 export interface QuestionAnswerArgs {
   inputs: {
     question: string;
     context: string;
+  };
+  options: {
+    wait_for_model?: boolean;
   };
 }
 
@@ -63,17 +35,29 @@ const instance = axios.create({
   baseURL: Constants.expoConfig.extra.baseURL,
   headers: {
     Authorization: "Bearer " + Constants.expoConfig.extra.huggingfaceKey,
-    timeout: 20000, // 20 seconds
   },
+  timeout: 20000, // 20 seconds
+});
+
+const onRetry = (retryCount, error) => {
+  console.log("retry", retryCount, error);
+};
+
+const retryDelay = () => 3000; // 3 seconds
+
+const retryCondition = () => true;
+
+axiosRetry(instance, {
+  retries: 3,
+  onRetry,
+  retryDelay,
+  retryCondition,
 });
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-const handleError = (error: Error | AxiosError) =>
-  console.log("API Error:", error);
-
-const handlePostError = (error: Error | AxiosError) => {
-  handleError(error);
+function handleError(error: Error | AxiosError) {
+  console.log(error);
   const defaultResponse: QuestionAnswerReturn = {
     answer: `I don't know. My API feels fuzzy.`,
     score: 0.5,
@@ -81,28 +65,13 @@ const handlePostError = (error: Error | AxiosError) => {
     end: 0,
   };
   return defaultResponse;
-};
+}
 
 const requests = {
-  // get: (url: string) => instance.get(url).then(responseBody.catch(handleError),
   post: (url: string, body: QuestionAnswerArgs) =>
-    instance.post(url, body).then(responseBody).catch(handlePostError),
-  // put: (url: string, body: {}) => instance.put(url, body).then(responseBody).catch(handleError),
-  // delete: (url: string) => instance.delete(url).then(responseBody).catch(handleError),
+    instance.post(url, body).then(responseBody).catch(handleError),
 };
 
 export const api = {
   getAnswer: (body): Promise<QuestionAnswerReturn> => requests.post("", body),
 };
-
-axios
-  .get("Web URL")
-  .then(function (response) {
-    // handle response
-  })
-  .catch(function (error) {
-    // handle error
-  })
-  .finally(function () {
-    // always executes at the last of any API call
-  });
