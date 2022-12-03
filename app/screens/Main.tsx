33dd1799d927eda80fcd555308d57d03904f158b
kurtvandusen <info@equationalapplications.com>
2022-12-03 08:59:18 -0500

    refactor: rename views directory as screens

--- app/screens/Main.tsx
+++ app/screens/Main.tsx
@@ -4,7 +4,6 @@ import { StyleSheet, SafeAreaView } from "react-native";
 import MessagesComponent from "../features/messages/MessagesComponent";
 
 export default function Main() {
-
   return (
     <SafeAreaView style={styles.container}>
       <MessagesComponent textInputStyle={styles.textInputStyle} />
