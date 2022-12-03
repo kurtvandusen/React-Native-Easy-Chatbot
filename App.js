dee627ed8b63f3d2c626605ca2143805371627d0
kurtvandusen <info@equationalapplications.com>
2022-12-03 08:58:33 -0500

    feat: add react error boundary

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { Provider } from "react-redux";
 
+import { ErrorBoundary } from "./app/screens/ErrorScreen/ErrorBoundary";
+import Main from "./app/screens/Main";
 import { store } from "./app/store";
-import Main from "./app/views/Main";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Main />
+      <ErrorBoundary catchErrors="always">
+        <Main />
+      </ErrorBoundary>
     </Provider>
   );
 }
